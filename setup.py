a8a2dd0a9996f0d97c4021127c24fe21343e8839
NickVTom <nicklasthomsenaau@gmail.com>
2024-02-26 11:06:38 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='assholes',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A custom utility package for statistical analysis and visualization.',
+    author='Nicklas',
+    author_email='nicklasthomsenaau@gmail.com',
+    url='https://github.com/NickVTom/assholes',
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'statsmodels',
+        # Add other dependencies here
+    ],
+)
